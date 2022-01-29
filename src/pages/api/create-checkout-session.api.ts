3bb9aa54d1f4f129bad6bcd4a211529ab02726bc
ruucm <ruucm.a@gmail.com>
2022-01-29 16:50:28 +0900

    create-checkout-session.api

---
+++ src/pages/api/create-checkout-session.api.ts
@@ -0,0 +1,56 @@
+import { getURL } from '@/utils'
+import { createOrRetrieveCustomer, stripe } from './webhooks/handlers'
+import { getSession } from 'next-auth/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const session = await getSession({ req })
+    const user = session.user
+
+    const { price, quantity = 1, metadata = {} } = req.body
+
+    console.log('user', user)
+    console.log('price', price)
+
+    try {
+      const customer = await createOrRetrieveCustomer({
+        // @ts-ignore
+        userId: user.userId,
+        email: user.email,
+      })
+
+      console.log('customer', customer)
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        billing_address_collection: 'required',
+        customer,
+        line_items: [
+          {
+            price,
+            quantity,
+          },
+        ],
+        mode: 'subscription',
+        allow_promotion_codes: true,
+        subscription_data: {
+          trial_from_plan: true,
+          metadata,
+        },
+        success_url: `${getURL()}/account`,
+        cancel_url: `${getURL()}/pricing`,
+      })
+
+      return res.status(200).json({ sessionId: session.id })
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ error: { statusCode: 500, message: err.message } })
+    }
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
+}
+
+export default handler
--- src/utils.ts
+++ src/utils.ts
@@ -6,17 +6,6 @@ export const toDateTime = (secs) => {
   return t
 }
 
-let stripePromise
-export const getStripe = () => {
-  if (!stripePromise) {
-    stripePromise = loadStripe(
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_LIVE ??
-        process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-    )
-  }
-  return stripePromise
-}
-
 export const postData = async ({ url, token, data = {} }) => {
   const res: any = await fetch(url, {
     method: 'POST',
@@ -30,4 +19,26 @@ export const postData = async ({ url, token, data = {} }) => {
   }
 
   return res.json()
+}
+
+//--------Strip payments------------
+let stripePromise
+export const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_LIVE ??
+        process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+    )
+  }
+  return stripePromise
+}
+
+export const getURL = () => {
+  const url =
+    process?.env?.URL && process.env.URL !== ''
+      ? process.env.URL
+      : process?.env?.VERCEL_URL && process.env.VERCEL_URL !== ''
+      ? process.env.VERCEL_URL
+      : process.env.BASE_URL
+  return url.includes('http') ? url : `https://${url}`
 }
